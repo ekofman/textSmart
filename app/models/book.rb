1c037ad8ec33f2922d4c79d6ede09c5f03644fd5
Eric Kofman <ericrkofman@gmail.com>
2014-02-21 00:14:43 -0800

    push to heroku

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ActiveRecord::Base
   attr_accessible :id, :user_id, :class_name, :book_name, :class_number, :subject, :price, :duration, :edition, :condition, :posting_date
   belongs_to :user
+
+  validates :book_name, presence: true
+  validates :class_name, presence: true
+  validates :price, numericality: true 
+  validates :price, presence: true
 end
